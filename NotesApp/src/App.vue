4e67bcb07b17503277e867d58b3cb0788b4dcc0f
Jiří Kofránek <kofranek@gmail.com>
2023-11-05 22:36:51 +0100

    overlay-začátek

--- NotesApp/src/App.vue
+++ NotesApp/src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <main>
+    <div class="overlay"></div>
     <div class="container">
       <header>
         <h1>Notes</h1>
         <button>+</button>
       </header>
-        <div class="card-container">
-          <card class="card"> 
-            <p class="main-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem soluta, possimus numquam aspernatur voluptatem cum.</p>
-            <p class="date">04/27/6853</p>
-          </card>
-          <card class="card"> 
-            <p class="main-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem soluta, possimus numquam aspernatur voluptatem cum.</p>
-            <p class="date">04/27/6853</p>
-          </card>
-        </div>
+    </div>
+    <div class="card-container">
+      <div class="card">
+        <p class="main-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem soluta,
+          possimus numquam aspernatur voluptatem cum.
+        </p>
+        <p class="date">04/27/6853</p>
+      </div>
+      <div class="card">
+        <p class="main-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem soluta,
+          possimus numquam aspernatur voluptatem cum.
+        </p>
+        <p class="date">04/27/6853</p>
+      </div>
     </div>
   </main>
 </template>
@@ -53,27 +60,37 @@ button {
   font-size: 20px;
 }
 
-.date{
-  font-size:12.5px;
-  font-weight:bold;
+.date {
+  font-size: 12.5px;
+  font-weight: bold;
 }
 
-card {
-  width:225px;
-  height:225px;
-  background-color:rgb(237,182,44);
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: rgb(237, 182, 44);
   padding: 10px;
-  border-radius:15px;
-  display:flex;
-  flex-direction:column;
-  justify-content:space-between;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   margin-right: 20px;
-  margin-bottom:20px;
+  margin-bottom: 20px;
 }
 
 .card-container {
-  display:flex;
-  flex-wrap:wrap;
+  display: flex;
+  flex-wrap: wrap;
+}
 
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index:10;
+  display:flex;
+  align-items:center;
+  justify-content:center;
 }
 </style>
