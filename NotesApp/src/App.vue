9d8fdd7105ae14de0a5f6fe1f583b91eed713e01
Jiří Kofránek <kofranek@gmail.com>
2023-11-07 00:22:17 +0100

    Počeštění dialogových tlačítek

--- NotesApp/src/App.vue
+++ NotesApp/src/App.vue
@@ -34,13 +34,13 @@ const addNote = () => {
         <textarea v-model.trim="newNote" name="note" id="note" cols="30" rows="10">
         </textarea>
         <p v-if="errorMessage">{{errorMessage}}</p>
-        <button @click="addNote()">Add Note</button>
-        <button @click="showModal = false" class="close">Close</button>
+        <button @click="addNote()">Přidej poznámku</button>
+        <button @click="showModal = false" class="close">Zpět</button>
       </div>
     </div>
     <div class="container">
       <header>
-        <h1>Notes</h1>
+        <h1>Poznámky</h1>
         <button @click="showModal = true">+</button>
       </header>
       <div class="card-container">
